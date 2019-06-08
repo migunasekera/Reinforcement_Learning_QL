a14a99b3dc92bb2380a561e7d6452bdc6c5f4afd
migunasekera <mi.gunasekera13@gmail.com>
2019-06-07 21:32:30 -0400

    Added optimal Q-table option, sped up RMSE processing on gym environments

--- QL.py
+++ QL.py
@@ -6,15 +6,14 @@ import evaluation
 import argparse
 from maze import *
 from evaluation import get_action_egreedy, evaluation
-import time
 import matplotlib.pyplot as plt
 
 
 class MDP(Maze):
     def __init__(self):
         super().__init__()
-        self.LEARNING_RATE = 0.1
-        self.DISCOUNT_RATE = 0.95
+        # self.LEARNING_RATE = 0.1
+        
 
 def RMSE_calc(Q_t, Q_opt):
     '''
@@ -23,21 +22,26 @@ def RMSE_calc(Q_t, Q_opt):
     return np.sqrt(np.sum((Q_opt - Q_t) ** 2) / len(Q_opt.flatten()))
 
 
-
-def train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = False):
+######################################################################################################
+#Maze
+######################################################################################################
+def train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE, findOptimal = False):
     '''
     env: Maze environment
     EPSILON: Determines epsilon for epsilon greedy algorithm. Higher the value, the less greedy [0,1]
     EPISODE_EVALUATION: Determines the number of episodes before we will evaluate how well the maze is doing
     EPISODES: Number of episodes that the learning algorith will run
     '''
+    name = "Maze"
+    DISCOUNT_RATE = 0.95
     tmpEval = []
     q_table  = np.random.uniform(low = 0, high = 3, size = (env.snum,env.anum))
+    print(findOptimal)
     if findOptimal is False:
         q_optimal = np.load('Q_maze.npy', allow_pickle = True)
     
     else:
-        # Throwaway thing, just to speed up the coding
+        # Throwaway thing, just to speed up the coding. Won't actually use it, but I don't want to use logic below in RMSE calculation
         q_optimal = np.copy(q_table)
     RMSE = []
     
@@ -54,6 +58,9 @@ def train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = 
             avg_step, avg_reward = evaluation(env,q_table)
             print(f"Avg Step: {avg_step} \nAvg Reward: {avg_reward}")
             tmpEval.append((episode,avg_step,avg_reward))
+            if findOptimal:
+                print(f"Saving episode {episode}...")
+                np.save(f"Optimal/{name}/{episode}.npy",q_table)
             
             time.sleep(1)
             continue
@@ -68,7 +75,7 @@ def train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = 
             if not done:
                 max_future_q = np.max(q_table[new_state])
                 current_q = q_table[(state,action)]
-                new_q = (1 - env.LEARNING_RATE) * current_q + env.LEARNING_RATE * (reward + env.DISCOUNT_RATE * max_future_q)
+                new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT_RATE * max_future_q)
                 q_table[(state,action)] = new_q # This was the critical step. You are updating the current Q state, not the future one!
             elif done: # Not too sure about how to give reward on the maze, and what the condition should be
                 q_table[(new_state,action)] = reward ##Not too sure what o do here
@@ -89,9 +96,9 @@ def train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = 
     return q_table, realEval, RMSE
 
 
-def evaluation_plot(realEval, RMSE):
+def evaluation_plot(realEval, RMSE, *args, save = False):
     fig, axs = plt.subplots(1,3)
-    fig.suptitle("Evaluation metrics for the Maze puzzle with current Q-Learning strategy")
+    fig.suptitle(f"Evaluation metrics for the {args[0]} puzzle with current Q-Learning strategy")
 
     episodes = realEval[...,0]
     steps = realEval[...,1]
@@ -121,10 +128,15 @@ def evaluation_plot(realEval, RMSE):
     axs[2].set_xlim((0,EPISODES))
     axs[2].set_ylim((0,np.max(RMSE_val)))
     axs[2].set_title("Root mean squared error")
+    if save:
+        fig.savefig(f"Results/{args[0]}_{int(args[1])}_{int(args[2])}.png")
 
     plt.show()
     print("Finished")
 
+######################################################################################################
+#GYM
+######################################################################################################
 def get_discrete_state(env, state, DISCRETE_OS_SIZE):
     
     discrete_window_size = (env.observation_space.high - env.observation_space.low)  / DISCRETE_OS_SIZE # Step sizes!
@@ -134,17 +146,18 @@ def get_discrete_state(env, state, DISCRETE_OS_SIZE):
     return tuple(discrete_state.astype(np.int))
 
 
-def train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = False):
+def train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE, findOptimal = False):
     '''
     Input: Env: Gym environment
 
     This will train a vlaue 
     '''
 
-    LEARNING_RATE = 0.1
+    # LEARNING_RATE = 0.1
     DISCOUNT_RATE = 0.95
     # EPSILON = 0.10
     # EPISODE_EVALUATION = 500 #Evaluate the current policy at these times. Plottable
+        
 
 
     
@@ -153,10 +166,17 @@ def train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = F
  
     q_table = np.random.uniform(low = -2, high = 0, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
     if findOptimal is False:
-        q_optimal = np.load('Q_car.npy', allow_pickle = True)
+        if env.observation_space.shape[0] == 2: ## Car environment
+            q_optimal = np.load('Q_car.npy', allow_pickle = True) # This is only for the car environemnt
+        elif env.observation_space.shape[0] == 6: ## Acrobot environment
+            q_optimal = np.load("Q_acrobot.npy")
     
     else:
-        # Throwaway thing, just to speed up the coding
+        # Throwaway thing, hacky way to solve it actually
+        if env.observation_space.shape[0] == 2: ## Car environment
+            name = "Car"
+        elif env.observation_space.shape[0] == 6: ## Acrobot environment
+            name = "Acrobot"
         q_optimal = np.copy(q_table)
     
 
@@ -186,6 +206,9 @@ def train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = F
             tmpEval.append((episode,avg_step,avg_reward))
             time.sleep(1)
             discrete_state = get_discrete_state(env, env.reset(),DISCRETE_OS_SIZE)
+            if findOptimal:
+                print(f"Saving episode {episode}...")
+                np.save(f"Optimal/{name}/{episode}.npy",q_table)
             render = True
         else:
             render = False
@@ -204,12 +227,17 @@ def train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON = 0.10, findOptimal = F
                 current_q = q_table[discrete_state + (action, )]
                 new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT_RATE * max_future_q)
                 q_table[discrete_state + (action, )] = new_q # This was the critical step. You are updating the current Q state, not the future one!
-                RMSE.append((episode, RMSE_calc(q_table,q_optimal)))
+                
             elif reward == 0:
             # elif new_state[0] >= env.goal_position: #env.goal_position = 0.5 for MountainCar-v0 environment # What about acrobot?
                 q_table[new_discrete_state + (action, )] = 0 # Automatically, this is an optimal state!
+                #################################################
+                #FIX THIS BUG
+                #################################################
+                
                 print("Done at Episode: ", episode)
             discrete_state = new_discrete_state
+        RMSE.append((episode, RMSE_calc(q_table,q_optimal)))
     realEval = np.array(tmpEval)
     RMSE = np.array(RMSE)
 
@@ -228,6 +256,10 @@ if __name__ == "__main__":
         '--acrobot', help = "Choose the Acrobat environment", action = "store_true"
     )
     parser.add_argument(
+        '--optimal', help = "Will run to create a Q-table that will be saved as an npy format. This is the optimal q-value that will be compared against", action = "store_true"
+
+    )
+    parser.add_argument(
         '--save', help = "Will save the value that is computed, for whatever learning algorithm you chose", action = "store_true"
 
     )
@@ -239,44 +271,66 @@ if __name__ == "__main__":
 
     if args.car:
         env = gym.make("MountainCar-v0")
-        EPISODES = 1000
-        EPISODE_EVALUATION = 200
+        EPISODES = 15000
+        EPISODE_EVALUATION = 500
+        EPSILON = 0.10
+        LEARNING_RATE = 0.1
+        
         name = "Q_car"
-        if args.save:
-            Q, _, _ = train_gym(env, EPISODES, EPISODE_EVALUATION, findOptimal= True)
-            np.save(name,Q)
+        if args.optimal:
+            Q, real_eval, _ = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE, findOptimal= True)
+            evaluation_plot(real_eval, _, name, EPSILON * 100, LEARNING_RATE * 100)
+            # np.save(name,Q)
+            # after 12000, it's pretty good
+        
+        elif args.save:
+            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+            evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100, save = True)
         else:
-            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION)
-            print(f"eval: {real_eval}")
-            print(f"RMSE: {RMSE}")
-            evaluation_plot(real_eval, RMSE)
+            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+            evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100)
+
         
 
     elif args.maze:
         name = "Q_maze"
-        EPISODES = 500
-        EPISODE_EVALUATION = 100
-        maze_env = MDP()
+        EPISODES = 5000
+        EPISODE_EVALUATION = 200
+        EPSILON = 0.10
+        LEARNING_RATE = 0.1
+        env = MDP()
         # for episode in EPISODES:
-        if args.save:
-            Q, _, _ = train_maze(maze_env, EPISODES, EPISODE_EVALUATION, findOptimal= True)
-            np.save(name,Q)
+        if args.optimal:
+            Q, real_eval, _ = train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE, findOptimal= True)
+            evaluation_plot(real_eval, _, name, EPSILON * 100, LEARNING_RATE * 100)
+            # np.save(name,Q)
+        elif args.save:
+                Q, real_eval, RMSE = train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+                evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100, save = True)
         else:
-            Q, real_eval, RMSE = train_maze(maze_env, EPISODES, EPISODE_EVALUATION)
-            evaluation_plot(real_eval, RMSE)        
+            Q, real_eval, RMSE = train_maze(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+            evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100)
+            # 3000 was a good one, w
+     
         pass
     elif args.acrobot:
         name = "Q_acrobot"
-        EPISODES = 15000
-        EPISODE_EVALUATION = 2000
+        EPISODES = 100
+        EPISODE_EVALUATION = 33
+        EPSILON = 0.10
+        LEARNING_RATE = 0.1
         env = gym.make("Acrobot-v1")
-        if args.save:
-            Q, _, _ = train_gym(env, EPISODES, EPISODE_EVALUATION, findOptimal= True)
-            np.save(name,Q)
-        else:
-            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION)
-            evaluation_plot(real_eval, RMSE)
+        if args.optimal:
+            Q, real_eval, _ = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE, findOptimal= True)
+            evaluation_plot(real_eval, _, name, EPSILON * 100, LEARNING_RATE * 100)
+            # np.save(name,Q)
         
+        elif args.save:
+            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+            evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100, save = True)
+        else:
+            Q, real_eval, RMSE = train_gym(env, EPISODES, EPISODE_EVALUATION, EPSILON, LEARNING_RATE)
+            evaluation_plot(real_eval, RMSE, name, EPSILON * 100, LEARNING_RATE * 100)
 
 
 
